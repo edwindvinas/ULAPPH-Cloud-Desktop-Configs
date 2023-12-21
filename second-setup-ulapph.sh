670993a599e9bd624906224a698f644783c1a099
Edwin D Vinas <edwin.d.vinas@gmail.com>
2023-12-21 13:06:00 +0800

    Update second-setup-ulapph.sh

--- second-setup-ulapph.sh
+++ second-setup-ulapph.sh
@@ -1,17 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "Installing needed packages..."
-pkg install git
-pkg install bc
-pkg install jq
-pkg install ffmpeg
-pkg install libpcap
-pkg install zsh
-pkg install cmatrix
-pkg install vim
-pkg install wget
-pkg install nginx
-pkg install termux-services
-pkg install cronie
-pkg install golang
-go version
+pkg install git --yes
+pkg install bc --yes
+pkg install jq --yes
+pkg install ffmpeg --yes
+pkg install libpcap --yes
+pkg install zsh --yes
+pkg install cmatrix --yes
+pkg install vim --yes
+pkg install wget --yes
+pkg install nginx --yes
+pkg install termux-services --yes
+pkg install cronie --yes
+pkg install golang --yes
+go version 
 
