94862cfdb31df85c7866e6e9356b29d51da6a640
Edwin D Vinas <edwin.d.vinas@gmail.com>
2023-12-21 12:59:51 +0800

    Create second-setup-ulapph.sh

---
+++ second-setup-ulapph.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "Installing needed packages..."
+pkg install git
+pkg install bc
+pkg install jq
+pkg install ffmpeg
+pkg install libpcap
+pkg install zsh
+pkg install cmatrix
+pkg install vim
+pkg install wget
+pkg install nginx
+pkg install termux-services
+pkg install cronie
+pkg install golang
+go version
+
